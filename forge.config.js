b30d98fc37f86c102e2e50dbabeffbb06533c7bd
goodkie <goodkie.com@gmail.com>
2026-05-16 20:25:17 -0400

    chore: bump version to v4.9.9 (final installer fix)

--- forge.config.js
+++ forge.config.js
@@ -4,26 +4,39 @@ module.exports = {
     name: 'XPIDER Browser',
     executableName: 'XPIDERBrowser',
     extraResource: ['./extensions'],
-    icon: './assets/icons/win/icon',  // .ico (Win) / .icns (Mac) 자동 선택
+    icon: './assets/icons/win/icon',
     appBundleId: 'com.xpider.browser',
     appVersion: require('./package.json').version,
     osxSign: process.env.CSC_LINK ? {} : undefined,
+    ignore: [
+      /^\/data($|\/)/,
+      /^\/backups($|\/)/,
+      /^\/_snapshots_full($|\/)/,
+      /^\/_RESTORE_POINTS($|\/)/,
+      /^\/restore_points($|\/)/,
+      /^\/snapshots($|\/)/,
+      /^\/portable_temp($|\/)/,
+      /^\/out($|\/)/,
+      /^\/xpider_/,
+      /^\/extension_logs/,
+      /^\/\.git($|\/)/,
+    ],
   },
   rebuildConfig: {},
 
   makers: [
     {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'XPIDERBrowser',
+        setupExe: `XPIDER-Browser-Windows-v${require('./package.json').version}-Setup.exe`,
+        setupIcon: './assets/icons/win/icon.ico',
+      },
+    },
+    {
       name: '@electron-forge/maker-zip',
       platforms: ['win32', 'darwin'],
       config: {},
-    },
-    {
-      name: '@electron-forge/maker-dmg',
-      config: {
-        format: 'ULFO',
-window: { width: 600, height: 400 },
-        icon: './assets/icons/mac/icon.icns'
-      }
     },
   ],
 
