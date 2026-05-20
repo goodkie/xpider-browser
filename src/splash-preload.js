3da411b017fc4896d8d959e4bed3999175be7852
goodkie <goodkie.com@gmail.com>
2026-05-20 05:41:55 -0400

    fix: load splash logo using direct relative path to resolve packaged ASAR file:/// resource access constraint

--- src/splash-preload.js
+++ src/splash-preload.js
@@ -7,9 +7,5 @@ contextBridge.exposeInMainWorld('splashAPI', {
   },
   onVersion: (callback) => {
     ipcRenderer.on('splash-version', (_, ver) => callback(ver));
-  },
-  // 로고 이미지 절대 경로 반환 (ASAR 내부 경로 포함)
-  getIconPath: () => {
-    return 'file://' + path.join(__dirname, 'assets', 'icon.png').replace(/\\/g, '/');
   }
 });
